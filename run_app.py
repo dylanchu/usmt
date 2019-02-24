fec98e959df0553a7d87e4cda856a9287d8e7025
dylanchu <chdy.uuid@gmail.com>
2019-02-25 00:50:59 +0800

    fix: uwsgi deployment

--- run_app.py
+++ run_app.py
@@ -5,8 +5,9 @@
 
 from app import create_app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.debug = True
     # handler = logging.FileHandler('flask.log')
     # app.logger.addHandler(handler)
