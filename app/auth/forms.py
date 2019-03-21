8315d4767f741a450593cfdc2db10f1ca1af606c
lyj <574473819@qq.com>
2019-03-21 19:23:45 +0800

    exception

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,15 +10,15 @@ from wtforms.validators import DataRequired,Email,EqualTo
 
 class RegisterForm(FlaskForm):
     #加入表单数据的验证
-    email = StringField('邮箱', validators=[DataRequired(message='邮箱不能为空'),Email('邮箱格式不正确')])
-    password = PasswordField('密码', validators=[DataRequired(message='密码不能为空')])
-    repeatpassword=PasswordField('确认密码', validators=[DataRequired(message='确认密码不能为空'),EqualTo('password',message="两次密码输入不一致")])
-    name = StringField('用户名', validators=[DataRequired(message='用户名不能为空')])
+    email = StringField('邮箱', validators=[DataRequired(),Email('邮箱格式不正确')])
+    password = PasswordField('密码', validators=[DataRequired()])
+    repeatpassword=PasswordField('确认密码', validators=[DataRequired(),EqualTo('password',message="两次密码输入不一致")])
+    name = StringField('用户名', validators=[DataRequired()])
     submit = SubmitField('提交')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('邮箱', validators=[DataRequired(message='邮箱不能为空'),Email('邮箱格式不正确')])
-    password = PasswordField('密码', validators=[DataRequired(message='密码不能为空')])
+    email = StringField('邮箱', validators=[DataRequired(),Email('邮箱格式不正确')])
+    password = PasswordField('密码', validators=[DataRequired()])
     remember = BooleanField('记住我')
     submit = SubmitField('提交')
